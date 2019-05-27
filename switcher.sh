a000a5282b470e1f52cbd0e959c4dd09fa5ae4aa
taha@asks2 <taha@chepec.se>
2019-05-27 17:46:04 +0200

    Fixed path so it matches my system.

--- switcher.sh
+++ switcher.sh
@@ -2,8 +2,8 @@
 caps="$(xset -q | grep Caps | sed -E "s/ //g;s/[0-9]*//g" | cut -d ":" -f 3)"
 if [ "$caps" == "off" ]; then
   echo "caps is off"
-  python3 ~/.i3/i3_wm_multi_disp_scripts/focus_next_display.py
+  python3 ~/.config/i3/i3_wm_multi_disp_scripts/focus_next_display.py
 else
   echo "caps is on"
-  python3 ~/.i3/i3_wm_multi_disp_scripts/switch_to_next_wk_in_project.py
+  python3 ~/.config/i3/i3_wm_multi_disp_scripts/switch_to_next_wk_in_project.py
 fi
